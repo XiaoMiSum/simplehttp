5efd8ecd858e82fd453a3aef2fc60059426dab76
XiaoMiSum <mi_xiao@qq.com>
2019-09-16 01:30:54 +0800

    移除了fastjson 依赖

--- src/test/java/xyz/migoo/simplehttp/test/SimpleHttpTest.java
+++ src/test/java/xyz/migoo/simplehttp/test/SimpleHttpTest.java
@@ -1,6 +1,5 @@
 package xyz.migoo.simplehttp.test;
 
-import com.alibaba.fastjson.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import xyz.migoo.simplehttp.HttpException;
@@ -14,18 +13,15 @@ class SimpleHttpTest {
 
     @Test
     void testGet() throws HttpException {
-        Assertions.assertTrue(Request.get("http://migoo.xyz").execute().body().contains("todos"));
+        Assertions.assertTrue(Request.get("http://migoo.xyz").execute().text().contains("todos"));
     }
 
     @Test
     void testPost() throws HttpException {
-        JSONObject body = new JSONObject();
-        body.put("userName", "test");
-        body.put("password", "123456");
-        body.put("sign", "12345");
-        Assertions.assertEquals(411, Request.post("http://migoo.xyz/api/login")
-                .bodyJson(body.toString())
-                .execute().jsonObject().getIntValue("status"));
+        String body = "{\"userName\":\"test\",\"password\":\"123456\",\"sign\": \"123456\"}";
+        Assertions.assertTrue(Request.post("http://migoo.xyz/api/login")
+                .bodyJson(body)
+                .execute().text().contains("411"));
     }
 
     @Test
@@ -34,5 +30,10 @@ class SimpleHttpTest {
                 .addHeader("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNTU4MTU4NTAyODA3IiwiZXhwIjoxNTY4NDMzMzQ2fQ.ZoxwmAnmb2zJB1Wne8bkPFpKWZKo1c-ggi6nIoT79NE")
                 .bodyJson("{\"id\":\"1558159224394\",\"title\":\"哈哈\",\"locked\":0,\"isDelete\":0}")
                 .execute().statusCode());
+    }
+
+    @Test()
+    void testException() {
+        Assertions.assertThrows(HttpException.class, () -> Request.put("http://127.0.0.1:8080").execute());
     }
 }
